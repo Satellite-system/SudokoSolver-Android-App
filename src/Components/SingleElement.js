ce3459a9e2cf7cbaa7ca107ed2743b621cc5af46
Satellite-system <adarshurmaliya@gmail.com>
2023-08-13 19:53:50 +0530

    Fix Styling of Cells

--- src/Components/SingleElement.js
+++ src/Components/SingleElement.js
@@ -8,16 +8,8 @@ import {
 
 const SingleElement = ({value, isDarkMode, handleCellChange, rowIndex, colIndex }) => {
 
-  // console.log("Value :: ", value)
-  const [num, setNum] = useState(value)
- 
-  const onChangeText = (val)=>{
-    console.log(">> ", val);
-    setNum(val);
-  }
-
   return (
-      <View style={{borderWidth: 0.5, borderColor: isDarkMode ? 'white' : 'black', flex:1, padding: 1}}>
+      <View style={{borderWidth: 0.5, borderColor: isDarkMode ? 'white' : 'black', flex:1, padding: 1, width: 40, height: 40, borderRightWidth: (colIndex === 2 || colIndex === 5 || colIndex === 8) ? 2 : 0.5, borderLeftWidth: colIndex === 0 ? 2 : 0.5, borderTopWidth: (rowIndex === 0 || rowIndex === 3 || rowIndex === 6) ? 2 : 0.5, borderBottomWidth: rowIndex === 8 ? 2 : 0.5 }}>
       <TextInput style={{color: isDarkMode ? 'white' : 'black', textAlign:'center'}} 
       value={value === 0 ? '' : value.toString()} // Convert to string 
       // onChangeText={onChangeText} 
